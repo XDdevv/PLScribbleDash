40cda9789031e9408c4ee29a19d1e216c2a4b055
XDdevv <undefineduser087@gmail.com>
2025-05-11 12:25:57 +0500

    Useless import's been removed

--- app/src/main/java/zed/rainxch/plscribbledash/presentation/screens/game_modes/one_round/utils/OneRoundGameState.kt
+++
@@ -1,9 +0,0 @@
-package zed.rainxch.plscribbledash.presentation.screens.game_modes.one_round.utils
-
-import zed.rainxch.plscribbledash.domain.model.ParsedPath
-
-sealed class OneRoundGameState {
-    data object PREVIEW : OneRoundGameState()
-    data object PLAY : OneRoundGameState()
-    data class FINISHED(val score: Int, val path: ParsedPath) : OneRoundGameState()
-}
